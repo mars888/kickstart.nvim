b5bde4a7dbe0d5e52938fd098169fa6916910b77
Marcel Strik <m.f.a.strik@gmail.com>
2024-08-13 14:52:24 +0200

    Added Harpoon

---
+++ lua/custom/plugins/harpoon.lua
@@ -0,0 +1,58 @@
+-- https://github.com/ThePrimeagen/harpoon/tree/harpoon2
+
+return {
+  'ThePrimeagen/harpoon',
+  branch = 'harpoon2',
+  requires = { { 'nvim-lua/plenary.nvim' } },
+  keys = {
+    {
+      '<leader>a',
+      function()
+        require('harpoon'):list():add()
+      end,
+    },
+    {
+      '<C-e>',
+      function()
+        local harpoon = require 'harpoon'
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end,
+    },
+    {
+      '<C-h>',
+      function()
+        require('harpoon'):list():select(1)
+      end,
+    },
+    {
+      '<C-t>',
+      function()
+        require('harpoon'):list():select(2)
+      end,
+    },
+    {
+      '<C-n>',
+      function()
+        require('harpoon'):list():select(3)
+      end,
+    },
+    {
+      '<C-s>',
+      function()
+        require('harpoon'):list():select(4)
+      end,
+    },
+    {
+      '<C-S-P>',
+      function()
+        require('harpoon'):list():prev()
+      end
+    },
+    {
+      '<C-S-N>',
+      function()
+        require('harpoon'):list():next()
+      end
+    },
+  },
+}
