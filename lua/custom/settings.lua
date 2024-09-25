a34fe6abced52eeffcfd833eaadc95c89d326759
Marcel Strik <m.f.a.strik@gmail.com>
2024-09-25 12:21:54 +0200

    Add title to window

--- lua/custom/settings.lua
+++ lua/custom/settings.lua
@@ -1,5 +1,8 @@
 -- [[ General settings ]]
 
+-- Set file as title.
+vim.o.title = true
+
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
