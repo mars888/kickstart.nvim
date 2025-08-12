690ff1d144fdfb59e623f79e52def88fbfd44329
Marcel Strik <m.f.a.strik@gmail.com>
2025-08-12 22:47:49 +0200

    Updated Leap to use lazy loading and simplified key setup

--- lua/custom/plugins/leap.lua
+++ lua/custom/plugins/leap.lua
@@ -1,17 +1,21 @@
 -- From http://www.lazyvim.org/extras/editor/leap#leapnvim
 return {
   'ggandor/leap.nvim',
-  enabled = true,
+  -- enabled = true,
+  keys = {
+    { 'gs', '<Plug>(leap-from-window)', desc = 'Leap from window' },
+    { '<C-s>', '<Plug>(leap)', desc = 'Leap' },
+  },
   config = function(_, opts)
     local leap = require 'leap'
     for k, v in pairs(opts) do
       leap.opts[k] = v
     end
     -- leap.add_default_mappings(true)
-    vim.keymap.set({ 'n', 'x', 'o' }, '<C-S>', '<Plug>(leap-forward)')
-    vim.keymap.set({ 'n', 'x', 'o' }, '<CS-S>', '<Plug>(leap)')
-    vim.keymap.set({ 'n', 'x', 'o' }, '<C-;>', '<Plug>(leap-backward)')
-    vim.keymap.set({ 'n', 'x', 'o' }, 'gs', '<Plug>(leap-from-window)')
+   -- vim.keymap.set({ 'n', 'x', 'o' }, '<C-S>', '<Plug>(leap-forward)')
+    -- vim.keymap.set({ 'n', 'x', 'o' }, '<CS-S>', '<Plug>(leap)')
+    -- vim.keymap.set({ 'n', 'x', 'o' }, '<C-;>', '<Plug>(leap-backward)')
+    -- vim.keymap.set({ 'n', 'x', 'o' }, 'gs', '<Plug>(leap-from-window)')
     -- vim.keymap.del({ 'x', 'o' }, 'x')
     -- vim.keymap.del({ 'x', 'o' }, 'X')
   end,
