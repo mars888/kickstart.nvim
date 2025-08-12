195992ddf909c38ccf8e7291fb3375f26f9d7da7
Marcel Strik <m.f.a.strik@gmail.com>
2025-08-12 22:47:19 +0200

    Changes to Snacks setup

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -22,23 +22,93 @@ return {
     explorer = { enabled = true },
     input = { enabled = true },
     lazygit = { enabled = true },
-    -- notifier = { enabled = true },
+    notifier = { enabled = true },
     scope = { enabled = true },
     scratch = { enabled = true },
+
+    picker = {
+      sources = {
+        explorer = {
+          win = {
+            list = {
+              keys = {
+                ['A'] = 'explorer_add_dotnet',
+              },
+            },
+          },
+          actions = {
+            explorer_add_dotnet = function(picker)
+              local dir = picker:dir()
+              local tree = require 'snacks.explorer.tree'
+              local actions = require 'snacks.explorer.actions'
+              local easydotnet = require 'easy-dotnet'
+
+              easydotnet.create_new_item(dir, function(item_path)
+                tree:open(dir)
+                tree:refresh(dir)
+                actions.update(picker, { target = item_path })
+              end)
+            end,
+          },
+        },
+      },
+    },
   },
   keys = {
     -- LazyGit
-    { "<leader>hlg", function() require("snacks").lazygit() end, desc = "LazyGit"  },
-    { "<leader>hll", function() require("snacks").lazygit.log() end, desc = "LazyGit log" },
-    { "<leader>hlf", function() require("snacks").lazygit.log_file() end, desc = "LazyGit log file" },
+    {
+      '<leader>hlg',
+      function()
+        require('snacks').lazygit()
+      end,
+      desc = 'LazyGit',
+    },
+    {
+      '<leader>hll',
+      function()
+        require('snacks').lazygit.log()
+      end,
+      desc = 'LazyGit log',
+    },
+    {
+      '<leader>hlf',
+      function()
+        require('snacks').lazygit.log_file()
+      end,
+      desc = 'LazyGit log file',
+    },
     -- Dim
-    { "<leader>td", toggle_snacks_dim, desc = "Toggle dim" },
+    { '<leader>td', toggle_snacks_dim, desc = 'Toggle dim' },
     -- Explorer
-    { "\\", function() require("snacks").explorer() end, desc = "Toggle explorer" },
+    {
+      '\\',
+      function()
+        require('snacks').explorer()
+      end,
+      desc = 'Toggle explorer',
+    },
     -- Notifier
-    { "<leader>gun", function() require("snacks").notifier() end, desc = "Notification history" },
+    {
+      '<leader>gun',
+      function()
+        require('snacks').notifier.show_history()
+      end,
+      desc = 'Notification history',
+    },
     -- Scratch buffer
-    { "<leader>.", function() require("snacks").scratch() end, desc = "Scratch buffer" },
-    { "<leader>S", function() require("snacks").scratch.select() end, desc = "Select scratch buffer" },
-  }
+    {
+      '<leader>.',
+      function()
+        require('snacks').scratch()
+      end,
+      desc = 'Scratch buffer',
+    },
+    {
+      '<leader>S',
+      function()
+        require('snacks').scratch.select()
+      end,
+      desc = 'Select scratch buffer',
+    },
+  },
 }
